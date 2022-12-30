e6fba6f2c11d1a208d79d27cc5ed9022f56fb806
owen <osn53387@uga.edu>
2022-12-30 12:36:13 -0500

    Added JS file

---
+++ cube.js
@@ -0,0 +1,12 @@
+/* colors to represent expertise 
+orange = java
+blue = typescript
+yellow = js
+etc...
+
+make each block on click pop up a form to then fill in some info
+
+after esatblished, refresh page, gridbox should be filled with color, on hover, displays name and main lang/langs
+
+on click of grid, popups the form with links and socials
+*/ 
